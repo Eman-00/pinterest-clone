4dfcef1a81bc53f82f07ea1527431637569d153f
Emanuel Marquis <49501465+Eman-00@users.noreply.github.com>
2022-06-28 11:19:40 -0300

    Implemented the User class
    Most of the User class features were added: account
    creation and deletion, login and sign out, and authentication
    state checking.
    
    - added instance singleton
    - added createAccount method
    - added deleteAccount method
    - added signIn method
    - added signOut method
    - added checkAuthState method
    - added setName method
    - added setEmail method
    - added setPassword method
    - added name getter
    - added email getter

--- app/lib/user.dart
+++ app/lib/user.dart
@@ -1,4 +1,74 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class User {
+  static User instance = User._User();
+
+  User._User();
+
+  String? get name => FirebaseAuth.instance.currentUser!.displayName;
   
+  String? get email => FirebaseAuth.instance.currentUser!.email;
+
+  Future<void> setName(String name) => FirebaseAuth.instance.currentUser!.updateDisplayName(name);
+  
+  Future<void> setEmail(String email) => FirebaseAuth.instance.currentUser!.updateEmail(email);
+  
+  Future<void> setPassword(String password) => FirebaseAuth.instance.currentUser!.updatePassword(password);
+
+  void createAccount(String name, String email, String password) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email, password: password
+      );
+      
+      await setName(name);
+      
+    } on FirebaseAuthException catch(e) {
+      print(e.code);
+    }
+  }
+
+  void deleteAccount() {
+    try {
+
+      FirebaseAuth.instance.currentUser!.delete();
+    } on FirebaseAuthException catch(e) {
+      print(e.code);
+    }
+  }
+
+  void signIn(String email, String password) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password
+      );
+    } on FirebaseAuthException catch(e) {
+      print(e.code);
+    }
+  }
+
+  void signOut() {
+    try {
+      FirebaseAuth.instance.signOut();
+    } on FirebaseAuthException catch (e) {
+      print(e.code);
+    }
+  }
+
+  // if state is false, it means the user is signed out, else the user is signed in.
+  void checkAuthState(void Function(bool state) getAuthState) {
+    try {
+      FirebaseAuth.instance.userChanges().listen((user) {
+          var state = user == null ? false : true;
+          getAuthState(state);
+      });
+    }
+    on FirebaseAuthException catch (e){
+      print(e.code);
+    } catch (e) {
+      print(e);
+    }
+  }
 }
